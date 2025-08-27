62af9953eab4ea6b3639b7c016d9f150374435f5
feliperocha021 <felipeoliveira02114@gmail.com>
2025-08-27 14:06:02 -0300

    feat: add jest coverage config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 import * as cookieParser from 'cookie-parser';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { SpelunkerModule } from 'nestjs-spelunker';
+//import { SpelunkerModule } from 'nestjs-spelunker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
